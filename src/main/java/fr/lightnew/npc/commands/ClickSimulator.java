88a208a97995b25c22e593c8c29f1b95b3183d0c
lightnew <louiis.hus@gmail.com>
2024-09-14 15:09:05 +0200

    Create ClickSimulator.java
    Simulate a click on npc with command /npc select

---
+++ src/main/java/fr/lightnew/npc/commands/ClickSimulator.java
@@ -0,0 +1,37 @@
+package fr.lightnew.npc.commands;
+
+import fr.lightnew.npc.LiteFP;
+import fr.lightnew.npc.entities.npc.NPCCreator;
+import fr.lightnew.npc.tools.ConsoleLog;
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.util.Vector;
+
+public class ClickSimulator {
+
+    private static Vector getDirection(Player player) {
+        return player.getLocation().getDirection();
+    }
+
+    public static NPCCreator getNPCFrontOfPlayer(Player player, double maxDistance) {
+        Location playerLocation = player.getLocation();
+        Vector direction = getDirection(player).normalize();
+
+        for (NPCCreator entity : LiteFP.list_npc.values()) {
+            if (entity.equals(player)) continue;
+
+            Vector toEntity = entity.getLocation().toVector().subtract(playerLocation.toVector());
+            double distance = toEntity.length();
+
+            if (distance <= maxDistance && direction.dot(toEntity.normalize()) > 0.9) {
+                return entity;
+            }
+        }
+
+        return null;
+    }
+}
+
+
